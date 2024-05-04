60d715740dac2389c43273b07704b860a0e609e8
vladdfe <168904763+vladdfe@users.noreply.github.com>
2024-05-04 14:41:07 +0300

    v2

--- PinPong.py
+++ PinPong.py
@@ -10,6 +10,19 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+class GameSprite(sprite.Sprite):
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        # Вызываем конструктор класса (Sprite):
+        super().__init__()
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (whight, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 while game:
     for e in event.get():
         if e tipe = QUIT:
