f62de7f896fec003d8213fe24298fc50a8eff8d2
vladdfe <168904763+vladdfe@users.noreply.github.com>
2024-05-04 15:30:06 +0300

    v3

--- PinPong.py
+++ PinPong.py
@@ -2,7 +2,7 @@ from pygame import *
 
 back = (200, 255, 255)
 win_width = 600
-win_height
+win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
@@ -10,6 +10,7 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
 class GameSprite(sprite.Sprite):
   # конструктор класса
     def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
@@ -21,12 +22,43 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
+ 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y <win_height -80:
+            self.rect.y += self.speed
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_W] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_S] and self.rect.y <win_height -80:
+            self.rect.y += self.speed
+
+racket1 = Player(r.png, 30, 200, 4, 50, 150)
+racket2 = Player(r.png, 30, 200, 4, 50, 150)
+ball = GameSprite(g.png, 200, 200, 4, 50, 50)
+
+speed_x = 3
+speed_y = 3
+
+
 while game:
     for e in event.get():
-        if e tipe = QUIT:
+        if e.tipe == QUIT:
             game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
     display.update()
     clock.tick(FPS)
