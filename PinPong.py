f77325e36cc881bb270ccea016673e7d46c33d68
vladdfe <168904763+vladdfe@users.noreply.github.com>
2024-05-04 14:17:41 +0300

    Add files via upload
    v1.0

---
+++ PinPong.py
@@ -0,0 +1,19 @@
+from pygame import *
+
+back = (200, 255, 255)
+win_width = 600
+win_height
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+while game:
+    for e in event.get():
+        if e tipe = QUIT:
+            game = False
+
+    display.update()
+    clock.tick(FPS)
